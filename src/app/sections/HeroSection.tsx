9599880e1d33e40479b426d1b20fffec4e8816a1
DiegoGraffi <diegograffigna1@gmail.com>
2025-06-28 15:42:17 -0300

    wip

--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="min-h-screen w-screen flex justify-center items-center p-10 flex-col gap-2 bg-radial-[at_50%_100%] from-white to-[#D2D7DC] overflow-hidden relative">
-      <h1 className="uppercase font-bold text-center text-xl border ">
-        Pagina de ruedas
+    <div className="min-h-screen w-screen flex items-center p-10 flex-col gap-2 bg-radial-[at_50%_100%] from-white to-[#D2D7DC] overflow-hidden relative">
+      <h1 className="uppercase text-center font-main text-[64px] mt-[20vh] max-w-[70%] text-[#094474] font-[900]">
+        Tecnología y Rendimiento en Movimiento
       </h1>
-      <p className="text-center text-lg max-w-[50%]">
+      <p className="text-center text-[18px] max-w-[50%] font-secondary text-[#20699B] font-[400]">
         esta es una muy linda pagina para que compres tus ruedas chinas
         favoritas, no tenemos imagenes de nuestros productos pero creenos que
         estan muy buenas
       </p>
 
-      {/* Contenedor exterior: posiciona y recorta la imagen a la mitad en la parte inferior */}
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 h-[400px] w-[1000px] overflow-hidden flex justify-center items-top">
-        {/* Contenedor interior: se encarga de la animación de giro */}
-        <div className="animate-[spin_15s_linear_infinite] h-[1000px] w-[1000px]">
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[50vw] max-w-[800px] h-[45vw] md:h-[40vw] lg:h-[35vw] xl:h-[25vw] max-h-[400px] overflow-hidden">
+        <div className="animate-[spin_15s_linear_infinite] w-full aspect-square">
           <Image
             src={RuedaImg}
             alt="Rueda Image"
-            className="h-[1000px] w-[1000px] origin-center"
+            width={800}
+            height={800}
+            className="h-full w-full object-contain"
           />
         </div>
       </div>
