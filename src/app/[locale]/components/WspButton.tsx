0817ce0ded45d356b030b43a59effde44437121c
JuanSimonM <merlojuansimon@gmail.com>
2025-07-11 13:14:37 -0300

    edited whatsapp number

--- src/app/[locale]/components/WspButton.tsx
+++ src/app/[locale]/components/WspButton.tsx
@@ -4,7 +4,7 @@ export default function WspButton() {
   return (
     <div className="group bg-[#25D366] rounded-full p-2 fixed bottom-6 right-6 z-50 shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer">
       <a
-        href="https://wa.me/1234567890"
+        href="https://wa.me/5492616815162"
         target="_blank"
         rel="noopener noreferrer"
       >
